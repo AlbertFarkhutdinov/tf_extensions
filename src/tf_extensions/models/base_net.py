02e13d14a2c0ec49ee208a3aba4c6f8a03d13ae5
AlbertFarkhutdinov <farhutdinovalbert@yandex.ru>
2025-03-22 13:49:04 +0400

    added BaseNet and BaseCNN

--- src/tf_extensions/models/base_net.py
+++ src/tf_extensions/models/base_net.py
@@ -1,40 +1,45 @@
-"""The module provides a custom segmentation network."""
-from __future__ import annotations
-
-from copy import deepcopy
-from dataclasses import dataclass, field
+"""The module provides a custom Tensorflow model."""
+import re
+from dataclasses import dataclass
 
 import tensorflow as tf
 from IPython import display
 
 from tf_extensions.auxiliary.base_config import BaseConfig
 from tf_extensions.auxiliary.custom_types import MaskType, TrainingType
-from tf_extensions.layers import ConvolutionalBlock
-from tf_extensions.layers import conv_configs as cc
 
 
 @dataclass
 class BaseNetConfig(BaseConfig):
     """
-    Configuration of a custom segmentation network.
+    Configuration of a custom network.
 
     Attributes
     ----------
-    conv_block_config : ConvolutionalBlockConfig
-        Configuration of convolutional blocks used in the network.
-    initial_filters_number : int
-        Number of filters in the first convolutional layer.
-    max_filters_number : int, optional
-        Maximum number of filters allowed in the network.
-        If None, no limit is applied.
+    name : str
+        Name of the neural network.
+    include_top : bool, optional
+        Whether to include the top layers in the model. Default is True.
 
     """
 
-    conv_block_config: cc.ConvolutionalBlockConfig = field(
-        default_factory=cc.ConvolutionalBlockConfig,
-    )
-    initial_filters_number: int = 16
-    max_filters_number: int = None
+    name: str = ''
+    include_top: bool = True
+
+    def __post_init__(self) -> None:
+        """Update configuration fields after initialization."""
+        if not self.name:
+            name = re.sub(
+                pattern='([a-z0-9])([A-Z])',
+                repl=r'\1_\2',
+                string=self.__class__.__name__.removesuffix('Config'),
+            )
+            name = re.sub(
+                pattern='([A-Z]+)([A-Z][a-z])',
+                repl=r'\1_\2',
+                string=name,
+            )
+            self.name = name.lower()  # noqa: WPS601
 
     def get_config_name(self) -> str:
         """
@@ -46,44 +51,28 @@ class BaseNetConfig(BaseConfig):
             A string representation of the configuration.
 
         """
-        config_name = 'input_neurons{0}'.format(self.initial_filters_number)
-        if self.max_filters_number:
-            config_name = '{config_name}_max_neurons{max_neurons}'.format(
-                config_name=config_name,
-                max_neurons=self.max_filters_number,
-            )
-        return '{config_name}_{conv_block_config_name}'.format(
-            config_name=config_name,
-            conv_block_config_name=self.conv_block_config.get_config_name(),
-        )
+        if self.include_top:
+            return self.name
+        return '{0}_without_top'.format(self.name)
 
 
 class BaseNet(tf.keras.Model):
     """
-    A custom segmentation network.
+    A custom Tensorflow model.
 
-    Parameters
+    Attributes
     ----------
-    config : cfg.CustomNetConfigType, optional
-        Configuration of the network.
-        If None, a default configuration is used.
-    include_top : bool, optional, default: True
-        Whether to include the final classification layer.
+    config : BaseNetConfig
+        Configuration of the model.
 
     """
 
-    def __init__(
-        self,
-        config: BaseNetConfig = None,
-        include_top: bool = True,
-    ) -> None:
+    config_type = BaseNetConfig
+
+    def __init__(self, **kwargs) -> None:
         """Initialize self. See help(type(self)) for accurate signature."""
+        self.config = self.__class__.config_type.from_kwargs(**kwargs)
         super().__init__()
-        self.include_top = include_top
-        self.config = config or BaseNetConfig()
-        kernel_size = self.config.conv_block_config.conv2d_config.kernel_size
-        if not kernel_size[0] % 2 or not kernel_size[1] % 2:
-            raise ValueError('Odd `kernel_size` is recommended.')
 
     def call(
         self,
@@ -92,15 +81,15 @@ class BaseNet(tf.keras.Model):
         mask: MaskType = None,
     ) -> tf.Tensor:
         """
-        Forward pass of the network.
+        Perform a forward pass through the network.
 
         Parameters
         ----------
         inputs : tf.Tensor
             The input tensor.
-        training : bool or tf.Tensor, optional
+        training : TrainingType
             Whether the model is in training mode.
-        mask : tf.Tensor or list of tf.Tensor, optional
+        mask : MaskType
             Mask tensor for specific layers.
 
         Returns
@@ -118,12 +107,12 @@ class BaseNet(tf.keras.Model):
         Parameters
         ----------
         input_shape : tuple of int
-            Shape of the input tensor (height, width, channels).
+            The shape of the input tensor, excluding the batch dimension.
 
         Returns
         -------
         tf.keras.Model
-            A Keras model instance.
+            A compiled Keras model.
 
         """
         input_layer = tf.keras.Input(shape=input_shape)
@@ -139,12 +128,12 @@ class BaseNet(tf.keras.Model):
         **kwargs,
     ) -> display.Image:
         """
-        Plot the model architecture.
+        Generate a visual representation of the model architecture.
 
         Parameters
         ----------
         input_shape : tuple of int
-            Shape of the input tensor (height, width, channels).
+            The shape of the input tensor, excluding the batch dimension.
 
         Returns
         -------
@@ -162,44 +151,6 @@ class BaseNet(tf.keras.Model):
 
     def summary(self, *args, **kwargs) -> None:
         """Print the summary of the model architecture."""
-        return self.build_graph(   # pragma: no cover
+        self.build_graph(   # pragma: no cover
             input_shape=kwargs.pop('input_shape'),
         ).summary(*args, **kwargs)
-
-    def get_convolutional_block(
-        self,
-        filter_scale: int,
-        kernel_size: tuple[int, ...] = None,
-        is_dropout_off: bool = False,
-    ) -> ConvolutionalBlock:
-        """
-        Return a convolutional block with the specified configuration.
-
-        Parameters
-        ----------
-        filter_scale : int
-            Scale factor for the number of filters in the convolutional layers.
-        kernel_size : tuple of int, optional
-            Kernel size for the convolutional layers.
-            If None, the default is used.
-        is_dropout_off : bool, optional, default: False
-            Whether to disable dropout in the convolutional block.
-
-        Returns
-        -------
-        ConvolutionalBlock
-            A configured convolutional block instance.
-
-        """
-        config = deepcopy(self.config.conv_block_config)
-        if kernel_size is not None:
-            config.conv2d_config.kernel_size = kernel_size
-        if is_dropout_off:
-            config.with_dropout = False
-        filters = self.config.initial_filters_number * filter_scale
-        if self.config.max_filters_number:
-            filters = min(self.config.max_filters_number, filters)
-        return ConvolutionalBlock(
-            filters=filters,
-            config=config,
-        )
--- tests/test_models/test_base_net.py
+++ tests/test_models/test_base_net.py
@@ -1,49 +1,17 @@
-import pytest
-
-from tf_extensions.layers import ConvolutionalBlock
-from tf_extensions.layers import conv_configs as cc
 from tf_extensions.models.base_net import BaseNet, BaseNetConfig
 
-base_net_properties = [
-    (
-        64,
-        (3, 3),
-        'relu',
-        True,
-        False,
-        False,
-        'glorot_uniform',
-    ),
-]
-def_conv2d = {
-    'kernel_size': (3, 3),
-    'padding': 'same',
-    'use_bias': True,
-    'kernel_initializer': 'glorot_uniform',
-}
-def_conv_block = {
-    'conv2d_config': def_conv2d,
-    'drop_rate': 0.5,
-    'layers_number': 2,
-    'activation': 'relu',
-    'with_bn': False,
-    'with_dropout': False,
-    'with_skipped': False,
-}
 def_base_net = {
-    'conv_block_config': def_conv_block,
-    'initial_filters_number': 16,
-    'max_filters_number': None,
+    'name': 'base_net',
+    'include_top': True,
 }
 
 
 class TestBaseNetConfig:
 
-    def test_init(self) -> None:
+    def test_init_default(self) -> None:
         config = BaseNetConfig()
-        assert config.conv_block_config == cc.ConvolutionalBlockConfig()
-        filters_number = config.initial_filters_number
-        assert filters_number == def_base_net['initial_filters_number']
+        assert config.name == 'base_net'
+        assert config.include_top
 
     def test_as_dict(self) -> None:
         config = BaseNetConfig()
@@ -54,196 +22,13 @@ class TestBaseNetConfig:
         assert config.from_dict(properties=def_base_net) == config
 
     def test_config_name(self) -> None:
-        conv_block_config = cc.ConvolutionalBlockConfig()
-        custom_seg_net_config = BaseNetConfig(
-            conv_block_config=conv_block_config,
-            initial_filters_number=32,
-            max_filters_number=64,
-        )
-        config_name = custom_seg_net_config.get_config_name()
-        assert config_name == 'input_neurons32_max_neurons64_relu2_kernel3x3'
+        config = BaseNetConfig()
+        config_name = config.get_config_name()
+        assert config_name == 'base_net'
 
 
 class TestBaseNet:
 
-    def test_init_without_args(self) -> None:
+    def test_init_default(self) -> None:
         model = BaseNet()
         assert isinstance(model.config, BaseNetConfig)
-
-    @pytest.mark.parametrize(
-        (
-            'filters',
-            'kernel',
-            'activation',
-            'bias',
-            'bn',
-            'dropout',
-            'initializer',
-        ),
-        base_net_properties,
-    )
-    def test_init(
-        self,
-        filters: int,
-        kernel: tuple[int, ...],
-        activation: str,
-        bias: bool,
-        bn: bool,
-        dropout: bool,
-        initializer: str,
-    ) -> None:
-        model = BaseNet(
-            config=BaseNetConfig(
-                initial_filters_number=filters,
-                conv_block_config=cc.ConvolutionalBlockConfig(
-                    conv2d_config=cc.Conv2DConfig(
-                        kernel_size=kernel,
-                        use_bias=bias,
-                        kernel_initializer=initializer,
-                    ),
-                    activation=activation,
-                    with_bn=bn,
-                    with_dropout=dropout,
-                ),
-            ),
-        )
-        assert model.config.initial_filters_number == filters
-        assert model.config.conv_block_config.layers_number == 2
-        assert model.config.conv_block_config.activation == activation
-        assert model.config.conv_block_config.with_bn == bn
-        assert model.config.conv_block_config.with_dropout == dropout
-        conv2d_config = model.config.conv_block_config.conv2d_config
-        assert conv2d_config.kernel_size == kernel
-        assert conv2d_config.padding == 'same'
-        assert conv2d_config.use_bias == bias
-        assert conv2d_config.kernel_initializer == initializer
-
-    @pytest.mark.parametrize(
-        ('filters', 'kernel_size'),
-        [
-            (64, (2, 2)),
-            (64, (4, 4)),
-        ],
-    )
-    def test_init_fail(
-        self,
-        filters: int,
-        kernel_size: tuple[int, ...],
-    ) -> None:
-        with pytest.raises(
-            ValueError,
-            match='Odd `kernel_size` is recommended.',
-        ):
-            BaseNet(
-                config=BaseNetConfig(
-                    initial_filters_number=filters,
-                    conv_block_config=cc.ConvolutionalBlockConfig(
-                        conv2d_config=cc.Conv2DConfig(
-                            kernel_size=kernel_size,
-                        ),
-                    ),
-                ),
-            )
-
-    @pytest.mark.parametrize(
-        (
-            'input_shape',
-            'filters',
-            'kernel',
-            'activation',
-            'bias',
-            'bn',
-            'dropout',
-            'initializer',
-        ),
-        [((128, 128, 1), *item) for item in base_net_properties],
-    )
-    def test_build_graph(
-        self,
-        input_shape: tuple[int, ...],
-        filters: int,
-        kernel: tuple[int, ...],
-        activation: str,
-        bias: bool,
-        bn: bool,
-        dropout: bool,
-        initializer: str,
-    ) -> None:
-        model = BaseNet(
-            config=BaseNetConfig(
-                initial_filters_number=filters,
-                conv_block_config=cc.ConvolutionalBlockConfig(
-                    conv2d_config=cc.Conv2DConfig(
-                        kernel_size=kernel,
-                        use_bias=bias,
-                        kernel_initializer=initializer,
-                    ),
-                    activation=activation,
-                    with_bn=bn,
-                    with_dropout=dropout,
-                ),
-            ),
-        )
-        graph = model.build_graph(input_shape=input_shape)
-        assert graph.inputs[0].shape == (None, *input_shape)
-        assert graph.outputs[0].shape == (None, *input_shape)
-
-    @pytest.mark.parametrize(
-        (
-            'filter_scale',
-            'conv_kernel',
-            'filters',
-            'kernel',
-            'act',
-            'bias',
-            'bn',
-            'dropout',
-            'init',
-        ),
-        [
-            *[(2, None, *item) for item in base_net_properties],
-            *[(2, (5, 5), *item) for item in base_net_properties],
-        ],
-    )
-    def test_get_convolutional_pair(
-        self,
-        filter_scale: int,
-        conv_kernel: tuple[int, ...],
-        filters: int,
-        kernel: tuple[int, ...],
-        act: str,
-        bias: bool,
-        bn: bool,
-        dropout: bool,
-        init: str,
-    ) -> None:
-        model = BaseNet(
-            config=BaseNetConfig(
-                initial_filters_number=filters,
-                conv_block_config=cc.ConvolutionalBlockConfig(
-                    conv2d_config=cc.Conv2DConfig(
-                        kernel_size=kernel,
-                        use_bias=bias,
-                        kernel_initializer=init,
-                    ),
-                    activation=act,
-                    with_bn=bn,
-                    with_dropout=dropout,
-                ),
-            ),
-        )
-        conv_block = model.get_convolutional_block(
-            filter_scale=filter_scale,
-            kernel_size=conv_kernel,
-        )
-        assert isinstance(conv_block, ConvolutionalBlock)
-        assert conv_block.filters == filters * filter_scale
-        assert conv_block.config.layers_number == 2
-        assert conv_block.config.activation == act
-        assert conv_block.config.with_bn == bn
-        assert conv_block.config.with_dropout == dropout
-        conv2d_config = conv_block.config.conv2d_config
-        assert conv2d_config.kernel_size == conv_kernel or kernel
-        assert conv2d_config.padding == 'same'
-        assert conv2d_config.use_bias == bias
-        assert conv2d_config.kernel_initializer == init
